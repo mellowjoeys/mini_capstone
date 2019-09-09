5027af7a85e4f43561742ea976779a635cf9562f
mellowjoeys <mellowjoey@gmail.com>
2019-09-08 21:45:48 -0500

    added validations

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -5,18 +5,42 @@ class Api::ProductsController < ApplicationController
   end
 
   def create
-    @product = {
+    @product = Product.new(
                 name: params[:name],
                 price: params[:price],
                 image_url: params[:image_url],
-                description: params[:description],
-               }
-    @product.save
-    render 'show.json.jb'
+                description: params[:description]
+               )
+    if @product.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @product.errors.full_messages, status: :unprocessable_entity}
+    end
   end
 
   def show
     @product = Product.find(params[:id])
     render 'show.json.jb'
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.description = params[:description] || @product.description
+    @product.image_url = params[:image_url] || @product.image_url
+
+    if @product.save
+      render 'show.json.jb'
+    else 
+      render json: {errors: @product.errors.full_messages, status: :unprocessable_entity}
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    render json: {store_owner: 'You break it, you buy it!'}
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,24 @@
 class Product < ApplicationRecord
+  validates :price, numericality: { :greater_than => 0 }
+  validates :price, presence: true
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :description, length: { minimum: 10, maximum: 500 } # {:in => 10..500} also works
+  validates :description, presence: true
+  validates :image_url, presence: true
+
+  def is_discounted?
+    # true if price < 300 
+    # else false
+    # price < 300 ? true : false # this line utilizes the ternary operator, leading to the happy or sad paths. 
+    price < 300
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
 end
